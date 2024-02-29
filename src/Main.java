4dbacbaca53864a390f2541950f29a9f252256ad
Slamboy2022 <95358356+Slamboy2022@users.noreply.github.com>
2024-03-01 00:33:49 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ public class Main {
     {
         Calc calc = new Calc();
         System.out.println("*** Калькулятор умеет выполнять операции");
-        System.out.println("*** Cложения строк, вычитания строки из строки, умножения строки на число и деления строки на число");
+        System.out.println("*** Cложение строк, вычитания строки из строки, умножения строки на число и деления строки на число");
         System.out.println("*** \"a\" + \"b\", \"a\" - \"b\", \"a\" * b, \"a\" / b");
-        System.out.println(calc.calcMat());
+        System.out.println("Ввод:");
+        String result = calc.calcMat();
+        System.out.println(result);
     }
 }
